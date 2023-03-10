1e9b44a5fe839ce179c431ce04fdf590675b84d3
joemar25 <joemar.cardino000@gmail.com>
2023-03-10 23:28:41 +0800

    magandang gabi - maya umaga na. Added ProductPrice Exploration and Cleaning

--- main.R
+++ main.R
@@ -66,23 +66,11 @@
 
 # ****************** Data Exploration ***************** #
 
-# (1) Install required packages if not already installed
+# Install required packages and Use Library
 
-if (!require(ISLR)) install.packages("ISLR")
 if (!require(ggplot2)) install.packages("ggplot2")
-if (!require(wordcloud)) install.packages("wordcloud")
-if (!require(tm)) install.packages("tm")
-if (!require(quanteda)) install.packages("quanteda")
 
-# (2) Import the libraries to be used
-
-library(ISLR)
 library(ggplot2)
-library(wordcloud)
-library(tm)
-library(quanteda)
-
-# (3) get file from the correct directory
 
 # load the data from the CSV file
 df <- read.csv("dataset.csv")
@@ -99,21 +87,24 @@ cat("obs. or rows->", obs_count)
 cat("var. or cols->", var_count)
 cat("column      ->", colm_name)
 
-# (4) Explore Data
+# explore sample of the data
 
-# (a) View the rows of the data-set (5, since it is hard to return all data)
+# view the rows of the data-set (5, since it is hard to return all data)
 head(df, 5)
 
-# present rows value of each column temporarily 
+# present rows value of each column temporarily
 head(df$kzProductName, 5)
-head(df$ProductPrice,  5)
-head(df$Rate,          5)
-head(df$Review,        5)
-head(df$Summary,       5)
-head(df$Sentiment,     5)
+head(df$ProductPrice, 5)
+head(df$Rate, 5)
+head(df$Review, 5)
+head(df$Summary, 5)
+head(df$Sentiment, 5)
 
 
-# (b) identify number of positive/negative/neutral
+
+# ******************** The Sentiment ****************** #
+
+# identify number of positive/negative/neutral
 
 num_pos <- sum(df$Sentiment == "positive")
 num_neg <- sum(df$Sentiment == "negative")
@@ -125,7 +116,7 @@ cat("number of sentiment negative: ", num_neg, "\n")
 cat("number of sentiment neutral : ", num_neu, "\n")
 
 # nani-kore
-cat("number of sentiment values  : [", total,   "] and obs. [", obs_count, "]\n")
+cat("number of sentiment values  : [", total, "] and obs. [", obs_count, "]\n")
 
 # - (1) why is that the total and obs don't match? - maybe there are null values
 sum(is.na(df$Sentiment))
@@ -141,12 +132,9 @@ num_other_sentiments <- sum(!(df$Sentiment %in% c("positive", "negative", "neutr
 cat("not (positive/negative/neutral) : ", num_other_sentiments, "\n")
 
 total <- num_other_sentiments + total
-cat("number of sentiment values  : [", total,   "] and obs. [", obs_count, "]\n")
+cat("number of sentiment values  : [", total, "] and obs. [", obs_count, "]\n")
 
-
-
-
-# plot
+# plot start
 options("scipen" = 100, "digits" = 4)
 
 # this is not case sensitive (positive can also be Positive)
@@ -163,7 +151,7 @@ ggplot(df_bar, aes(x = Sentiment, y = Count, fill = Sentiment)) +
 
 
 
-# kzProductName
+# ****************** The kzProductName **************** #
 
 # Create a frequency table of the kzProductName column
 freq_table <- table(df$kzProductName)
@@ -177,30 +165,27 @@ print(top_3_values)
 
 
 
+# ********************* The Price ********************* #
 
+# if the ProductPrice is in character then normal_price would be 0
+normal_price <- sum(is.na(df$ProductPrice))
+normal_price
 
+# converting to double
+df$ProductPrice <- as.double(df$ProductPrice)
 
-
-
-
-
-# Price
-
-# Convert ProductPrice to numeric type
-df$ProductPrice <- as.numeric(df$ProductPrice)
-
-# Count the number of missing values in ProductPrice
-num_missing <- sum(is.na(df$ProductPrice))
-
-# Count the number of non-missing values in ProductPrice
+# count the number of non-missing values in ProductPrice
 num_valid <- sum(!is.na(df$ProductPrice))
 
-# Compute the number of values that don't match the numeric format
-num_mismatched <- sum(is.na(as.numeric(df$ProductPrice)))
+# count the number of missing values in ProductPrice
+num_missing <- sum(is.na(df$ProductPrice))
 
-# Compute the mean and standard deviation of ProductPrice (excluding missing and mismatched values)
-mean_price <- mean(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.numeric(df$ProductPrice))])
-sd_price <- sd(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.numeric(df$ProductPrice))])
+# count the number of values that don't match the numeric format
+num_mismatched <- sum(is.na(as.double(df$ProductPrice))) - num_missing
+
+# compute the mean and standard deviation of ProductPrice (excluding missing and mismatched values)
+mean_price <- mean(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.double(df$ProductPrice))])
+sd_price <- sd(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.double(df$ProductPrice))])
 
 # Print the results
 cat("Valid:", num_valid, "\n")
@@ -208,6 +193,76 @@ cat("Mismatched:", num_mismatched, "\n")
 cat("Missing:", num_missing, "\n")
 cat("Mean:", mean_price, "\n")
 cat("Std. Deviation:", sd_price, "\n")
+
+# questioning the result. Why are there so many missing values?
+
+# check the totality of valid and missing to the obs.
+t <- num_valid + num_missing
+
+cat("valid + missing =", t, "\n")
+cat("obs. =", obs_count, "\n")
+
+# ----> the problem is in the num_missing when df$ProductPrice is converted to Double
+# value are not cleaned yet, but it convert's the values to double making the invalid numbers NaN
+
+# Present the missing values in ProductPrice
+missing_values <- df$ProductPrice[which(is.na(df$ProductPrice))]
+missing_values
+
+# calculate the total price
+total_price <- sum(df$ProductPrice, na.rm = TRUE)
+
+# format the total price with commas and a dollar sign
+formatted_price <- paste0("$", format(total_price, big.mark = ",", scientific = FALSE))
+
+# print the formatted total price
+formatted_price # this is suppoed to be $895,321,820 when data is cleaned for ProductPrice
+
+# find the lowest price
+lowest_price <- min(df$ProductPrice, na.rm = TRUE)
+
+# find the highest price
+highest_price <- max(df$ProductPrice, na.rm = TRUE)
+
+# print the lowest and highest prices
+cat("The lowest price is", lowest_price, "\n")
+cat("The highest price is", highest_price, "\n")
+
+# convert ProductPrice to numeric type
+df$ProductPrice <- as.double(df$ProductPrice)
+
+# remove rows with missing values in ProductPrice
+df <- df[!is.na(df$ProductPrice), ]
+
+# define breaks based on available values
+breaks <- seq(min(unique(df$ProductPrice)),
+    max(unique(df$ProductPrice)),
+    length.out = 10
+)
+
+# create histogram with customized options
+hist(df$ProductPrice,
+    breaks = breaks,
+    col = "lightblue",
+    border = "white",
+    main = "Distribution of Product Prices",
+    xlab = "Price (in USD)",
+    ylab = "Count",
+    xlim = c(min(breaks), max(breaks)),
+    ylim = c(0, max(hist(df$ProductPrice, breaks = breaks)$counts) * 1.1)
+)
+
+# price representation - will be represented again when it is cleaned to see differences
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -218,6 +273,82 @@ cat("Std. Deviation:", sd_price, "\n")
 
 
 # ****************** Data Pre-Process ***************** #
+
+# ********************* The Price ********************* #
+
+# define a regular expression to extract numeric values from strings
+pattern <- "\\d+(\\.\\d+)?"
+
+# clean the ProductPrice column by extracting numeric values
+# convert the value to double
+df$ProductPrice <- sapply(df$ProductPrice, function(x) {
+    ifelse(grepl(pattern, x), as.double(regmatches(x, regexpr(pattern, x))), NA)
+})
+
+# check for missing values and inspect corresponding rows
+missing_indices <- which(is.na(df$ProductPrice))
+df[missing_indices, "ProductPrice"]
+
+num_missing <- sum(is.na(df$ProductPrice))
+num_missing
+
+num_mismatched <- sum(is.na(as.double(df$ProductPrice))) - num_missing
+num_mismatched
+
+num_valid <- sum(!is.na(df$ProductPrice))
+num_valid
+
+# compute the mean and standard deviation of ProductPrice (excluding missing and mismatched values)
+mean_price <- mean(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.double(df$ProductPrice))])
+sd_price <- sd(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.double(df$ProductPrice))])
+
+# print the results
+cat("Valid:", num_valid, "\n")
+cat("Mismatched:", num_mismatched, "\n")
+cat("Missing:", num_missing, "\n")
+cat("Mean:", mean_price, "\n")
+cat("Std. Deviation:", sd_price, "\n")
+
+# calculate the total price
+total_price <- sum(df$ProductPrice, na.rm = TRUE)
+
+# format the total price with commas and a dollar sign
+formatted_price <- paste0("$", format(total_price, big.mark = ",", scientific = FALSE))
+
+# print the formatted total price
+formatted_price
+
+# find the lowest price
+lowest_price <- min(df$ProductPrice, na.rm = TRUE)
+
+# find the highest price
+highest_price <- max(df$ProductPrice, na.rm = TRUE)
+
+# print the lowest and highest prices
+cat("The lowest price is", lowest_price, "\n")
+cat("The highest price is", highest_price, "\n")
+
+# define breaks based on available values
+breaks <- seq(min(unique(df$ProductPrice)),
+    max(unique(df$ProductPrice)),
+    length.out = 10
+)
+
+# create histogram with customized options
+hist(df$ProductPrice,
+    breaks = breaks,
+    col = "lightblue",
+    border = "white",
+    main = "Distribution of Product Prices",
+    xlab = "Price (in USD)",
+    ylab = "Count",
+    xlim = c(min(breaks), max(breaks)),
+    ylim = c(0, max(hist(df$ProductPrice, breaks = breaks)$counts) * 1.1)
+)
+
+
+
+# ********************* Other Col ********************* #
 
 # itama ang mga data types ng columns
 # linisin ang mga columns data
