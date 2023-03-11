f67d2f8fc8624b3d539aa6e31d7521a937e8aa6d
joemar25 <joemar.cardino000@gmail.com>
2023-03-12 02:00:39 +0800

    Updated main R code

--- main.R
+++ main.R
@@ -66,30 +66,34 @@
 
 # ****************** Data Exploration ***************** #
 
-# Install required packages and Use Library
-
+# install required packages and Use Library
 if (!require(ggplot2)) install.packages("ggplot2")
-
 library(ggplot2)
+
+if (!require(dplyr)) install.packages("dplyr")
+library(dplyr)
 
 # load the data from the CSV file
 df <- read.csv("dataset.csv")
 
+# set the width of the console to 120 characters
+options(width = 120)
+
 # check the structure of the data frame
 str(df)
+
+# checking dataset of each columns, and we can see the columns if has missing values
+summary(df)
 
 # obs and var
 obs_count <- nrow(df)
 var_count <- ncol(df)
 colm_name <- colnames(df)
+cat("obs. or rows->", obs_count, "\n")
+cat("var. or cols->", var_count, "\n")
+cat("column      ->", colm_name, "\n")
 
-cat("obs. or rows->", obs_count)
-cat("var. or cols->", var_count)
-cat("column      ->", colm_name)
-
-# explore sample of the data
-
-# view the rows of the data-set (5, since it is hard to return all data)
+# explore sample of the data (5 only since data is large)
 head(df, 5)
 
 # present rows value of each column temporarily
@@ -100,54 +104,29 @@ head(df$Review, 5)
 head(df$Summary, 5)
 head(df$Sentiment, 5)
 
-
-
 # ******************** The Sentiment ****************** #
 
-# identify number of positive/negative/neutral
-
-num_pos <- sum(df$Sentiment == "positive")
-num_neg <- sum(df$Sentiment == "negative")
-num_neu <- sum(df$Sentiment == "neutral")
-total <- num_pos + num_neg + num_neu
-
-cat("number of sentiment positive: ", num_pos, "\n")
-cat("number of sentiment negative: ", num_neg, "\n")
-cat("number of sentiment neutral : ", num_neu, "\n")
-
-# nani-kore
-cat("number of sentiment values  : [", total, "] and obs. [", obs_count, "]\n")
-
-# - (1) why is that the total and obs don't match? - maybe there are null values
-sum(is.na(df$Sentiment))
-
-# - (2) why is that the total and obs don't match? - maybe there are not equal to positive/negative/neutral
-other_sentiments <- df$Sentiment[!(df$Sentiment %in% c("positive", "negative", "neutral"))]
-
-# truth -> we only check for the number of values of the lowercase(positive, negativem neutral)
 unique(df$Sentiment)
 
-# get the other sentiments count
-num_other_sentiments <- sum(!(df$Sentiment %in% c("positive", "negative", "neutral")))
-cat("not (positive/negative/neutral) : ", num_other_sentiments, "\n")
-
-total <- num_other_sentiments + total
-cat("number of sentiment values  : [", total, "] and obs. [", obs_count, "]\n")
+sentiment_counts <- df %>% count(Sentiment)
+sentiment_counts$total <- obs_count
+sentiment_counts$Percentage <- paste0(round((sentiment_counts$n / sentiment_counts$total) * 100, 2), "%")
+sentiment_counts
 
 # plot start
 options("scipen" = 100, "digits" = 4)
 
 # this is not case sensitive (positive can also be Positive)
 df_bar <- data.frame(
-    Sentiment = c("Positive", "Negative", "Neutral"),
-    Count = c(num_pos, num_neg, num_neu)
+  Sentiment = c("Positive", "Negative", "Neutral"),
+  Count = c(sentiment_counts$n[sentiment_counts$Sentiment == "positive"], sentiment_counts$n[sentiment_counts$Sentiment == "negative"], sentiment_counts$n[sentiment_counts$Sentiment == "neutral"])
 )
 
 ggplot(df_bar, aes(x = Sentiment, y = Count, fill = Sentiment)) +
-    geom_bar(stat = "identity") +
-    ggtitle("Sentiment Analysis") +
-    xlab("Sentiment") +
-    ylab("Count")
+  geom_bar(stat = "identity") +
+  ggtitle("Sentiment Analysis") +
+  xlab("Sentiment") +
+  ylab("Count")
 
 
 
@@ -159,59 +138,52 @@ freq_table <- table(df$kzProductName)
 # Sort the table in descending order
 sorted_table <- sort(freq_table, decreasing = TRUE)
 
-# Select the top 3 values | determine most common word to occur
+# Select the top 3 values
 top_3_values <- head(sorted_table, n = 3)
-print(top_3_values)
+
+# Calculate total number of values in the column
+total <- sum(freq_table)
+
+# Calculate percentage for each of the top 3 values
+percentages <- round((top_3_values / total) * 100, 2)
+
+# Calculate the percentage of all other values
+other_percentage <- 100 - sum(percentages)
+
+# Combine the values and percentages into a data frame
+top_3_df <- data.frame(
+  kzProductName = c(names(top_3_values), "Others"),
+  Frequency = c(top_3_values, sum(freq_table) - sum(top_3_values)),
+  Percentage = c(paste0(percentages, "%"), paste0(other_percentage, "%"))
+)
+
+# Format the output to use fewer decimal places
+top_3_df$Percentage <- format(round(as.numeric(sub("%", "", top_3_df$Percentage)) / 100, 2), nsmall = 2, digits = 2)
+
+# Print the resulting data frame
+print(top_3_df, row.names = FALSE)
+
 
 
 
 # ********************* The Price ********************* #
 
-# if the ProductPrice is in character then normal_price would be 0
 normal_price <- sum(is.na(df$ProductPrice))
-# you cannot sum a number that is not numeric...
-normal_price
-
-# converting to double - warning ahead
-df$ProductPrice <- as.double(df$ProductPrice)
-
-# count the number of non-missing values in ProductPrice
 num_valid <- sum(!is.na(df$ProductPrice))
-
-# count the number of missing values in ProductPrice
-num_missing <- sum(is.na(df$ProductPrice))
-
-# count the number of values that don't match the numeric format
-num_mismatched <- sum(is.na(as.double(df$ProductPrice))) - num_missing
-
-# compute the mean and standard deviation of ProductPrice (excluding missing and mismatched values)
-mean_price <- mean(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.double(df$ProductPrice))])
-sd_price <- sd(df$ProductPrice[!is.na(df$ProductPrice) & !is.na(as.double(df$ProductPrice))])
+num_outlier <- sum(df$ProductPrice < 0)
 
 # Print the results
-cat("Valid:", num_valid, "\n")
-cat("Mismatched:", num_mismatched, "\n")
-cat("Missing:", num_missing, "\n")
-cat("Mean:", mean_price, "\n")
-cat("Std. Deviation:", sd_price, "\n")
+cat("Number of normal prices: ", normal_price, "\n")
+cat("Number of valid prices: ", num_valid, "\n")
+cat("Number of outlier prices: ", num_outlier, "\n")
 
-# questioning the result. Why are there so many missing values?
+# see outlier prices
+outlier_prices <- df$ProductPrice[df$ProductPrice < 0]
+outlier_prices
 
-# check the totality of valid and missing to the obs.
-t <- num_valid + num_missing
-
-cat("valid + missing =", t, "\n")
-cat("obs. =", obs_count, "\n")
-
-# ----> the problem is in the num_missing when df$ProductPrice is converted to Double
-# value are not cleaned yet, but it convert's the values to double making the invalid numbers NaN
-
-# Present the missing values in ProductPrice
-missing_values <- df$ProductPrice[which(is.na(df$ProductPrice))]
-# what are the values that are not numeric
-unique(missing_values)
-
-# calculate the total price
+# na.rm = TRUE argument tells R to ignore missing values when calculating the sum
+# warning is present since it has outliers
+df$ProductPrice <- as.double(df$ProductPrice)
 total_price <- sum(df$ProductPrice, na.rm = TRUE)
 
 # format the total price with commas and a dollar sign
@@ -230,31 +202,120 @@ highest_price <- max(df$ProductPrice, na.rm = TRUE)
 cat("The lowest price is", lowest_price, "\n")
 cat("The highest price is", highest_price, "\n")
 
-# convert ProductPrice to numeric type
-df$ProductPrice <- as.double(df$ProductPrice)
-
 # remove rows with missing values in ProductPrice
 df <- df[!is.na(df$ProductPrice), ]
 
 # define breaks based on available values
 breaks <- seq(min(unique(df$ProductPrice)),
-    max(unique(df$ProductPrice)),
-    length.out = 10
+  max(unique(df$ProductPrice)),
+  length.out = 10
 )
 
 # create histogram with customized options
 hist(df$ProductPrice,
-    breaks = breaks,
-    col = "lightblue",
-    border = "white",
-    main = "Distribution of Product Prices",
-    xlab = "Price (in USD)",
-    ylab = "Count",
-    xlim = c(min(breaks), max(breaks)),
-    ylim = c(0, max(hist(df$ProductPrice, breaks = breaks)$counts) * 1.1)
+  breaks = breaks,
+  col = "lightblue",
+  border = "white",
+  main = "Distribution of Product Prices",
+  xlab = "Price (in USD)",
+  ylab = "Count",
+  xlim = c(min(breaks), max(breaks)),
+  ylim = c(0, max(hist(df$ProductPrice, breaks = breaks)$counts) * 1.1)
 )
 
 # price representation - will be represented again when it is cleaned to see differences
+
+
+
+
+# ********************** The Rate ********************* #
+
+# Summary statistics for Rate
+summary(df$Rate)
+
+# see outlier prices
+any(df$Rate < 0)
+
+outlier_rate <- df$Rate[df$Rate < 1 | df$Rate > 5]
+outlier_rate
+
+# convert to make graphing
+df$Rate <- as.numeric(df$Rate)
+
+# Visualize the distribution of Rate using a histogram
+ggplot(df, aes(Rate)) +
+  geom_histogram(binwidth = 1, fill = "blue", alpha = 0.5) +
+  labs(x = "Rating", y = "Count") +
+  ggtitle("Distribution of Ratings")
+
+# Calculate the mean rating
+mean(df$Rate, na.rm = TRUE)
+
+# Calculate the median rating
+median(df$Rate, na.rm = TRUE)
+
+# Calculate the mode rating
+
+if (!require(modeest)) install.packages("modeest")
+
+library(modeest)
+
+mlv(df$Rate)
+
+ggplot(df, aes(y = Rate)) +
+  geom_boxplot(fill = "blue", alpha = 0.5) +
+  labs(y = "Rating") +
+  ggtitle("Distribution of Ratings")
+
+
+
+
+
+# Summary statistics for Review length
+summary(df$Review)
+
+# Top 10 most frequent Review
+top_review <- df %>%
+  count(Review, sort = TRUE) %>%
+  head(10)
+top_review
+
+# Bar plot of top 10 most frequent Review
+ggplot(top_review, aes(x = Review, y = n)) +
+  geom_col() +
+  xlab("Review") +
+  ylab("Count") +
+  ggtitle("Top 10 most frequent Review")
+
+
+
+
+
+
+
+# Summary statistics for Summary
+summary(df$Summary)
+
+# Top 10 most common summaries
+top_summary <- df %>%
+  count(Summary, sort = TRUE) %>%
+  head(10)
+top_summary
+
+# Bar plot of top 10 most frequent Review
+ggplot(top_summary, aes(x = Summary, y = n)) +
+  geom_col() +
+  xlab("Summary") +
+  ylab("Count") +
+  ggtitle("Top 10 most frequent Summary")
+
+
+
+
+
+
+
+
 
 
 
@@ -276,8 +337,35 @@ hist(df$ProductPrice,
 
 # ****************** Data Pre-Process ***************** #
 
-# fresh start
+# start
 df <- read.csv("dataset.csv")
+
+# checking
+summary(df)
+
+# Check for missing values
+sum(is.na(df))
+
+# *********************** General ********************* #
+
+# rename the kzProductName column to ProductName
+colnames(df)[colnames(df) == "kzProductName"] <- "ProductName"
+
+# verify that the column has been renamed
+colnames(df)
+
+
+
+# ****************# The ProductName #****************** #
+
+
+
+
+
+
+
+
+
 
 # ********************* The Price ********************* #
 
@@ -287,7 +375,7 @@ pattern <- "\\d+(\\.\\d+)?"
 # clean the ProductPrice column by extracting numeric values
 # convert the value to double
 df$ProductPrice <- sapply(df$ProductPrice, function(x) {
-    ifelse(grepl(pattern, x), as.double(regmatches(x, regexpr(pattern, x))), NA)
+  ifelse(grepl(pattern, x), as.double(regmatches(x, regexpr(pattern, x))), NA)
 })
 
 # check for missing values and inspect corresponding rows
@@ -335,20 +423,20 @@ cat("The highest price is", highest_price, "\n")
 
 # define breaks based on available values
 breaks <- seq(min(unique(df$ProductPrice)),
-    max(unique(df$ProductPrice)),
-    length.out = 10
+  max(unique(df$ProductPrice)),
+  length.out = 10
 )
 
 # create histogram with customized options
 hist(df$ProductPrice,
-    breaks = breaks,
-    col = "lightblue",
-    border = "white",
-    main = "Distribution of Product Prices",
-    xlab = "Price (in USD)",
-    ylab = "Count",
-    xlim = c(min(breaks), max(breaks)),
-    ylim = c(0, max(hist(df$ProductPrice, breaks = breaks)$counts) * 1.1)
+  breaks = breaks,
+  col = "lightblue",
+  border = "white",
+  main = "Distribution of Product Prices",
+  xlab = "Price (in USD)",
+  ylab = "Count",
+  xlim = c(min(breaks), max(breaks)),
+  ylim = c(0, max(hist(df$ProductPrice, breaks = breaks)$counts) * 1.1)
 )
 
 
@@ -357,64 +445,3 @@ hist(df$ProductPrice,
 
 # itama ang mga data types ng columns
 # linisin ang mga columns data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# testing for data pre-processing TESTING LNG POOOO************************
-library(caret)
-
-
-# DATA SPLITTING
-# Split data into training and testing subsets
-set.seed(123) # set a seed for reproducibility
-trainIndexdf <- createDataPartition(df$Sentiment, p = 0.8, list = FALSE)
-traindf <- df[trainIndexdf, ]
-testdf <- df[-trainIndexdf, ]
-
-
-# PRE-PROCESSING:
-# Pre-process data
-preProcdf <- preProcess(traindf[, -5], method = c("center", "scale", "pca"))
-
-# Apply pre-processing to training and testing data
-trainTransformeddf <- predict(preProcdf, traindf[, -5])
-testTransformeddf <- predict(preProcdf, testdf[, -5])
-
-
-# Train a predictive model
-modeldf <- train(Sentiment ~ ., data = trainTransformeddf, method = "rf", trControl = trainControl(method = "cv"))
-
-
-# Set up cross-validation
-ctrldf <- trainControl(method = "cv", number = 5)
-
-
-# Make predictions on test data
-predictionsdf <- predict(modeldf, testdf)
-
-
-# Compute confusion matrix and other performance measures
-cmdf <- confusionMatrix(predictionsdf, testdf$Sentiment)
-cmdf
